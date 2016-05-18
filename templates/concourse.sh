d07e3b1540cdc76af7468cf4a38b4f0f69c99089
Kinnan Kwok <kkwoker@gmail.com>
2016-05-17 21:24:20 -0700

    use absolute paths

--- templates/concourse.sh
+++ templates/concourse.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd /opt/concourse
-./concourse_linux_amd64 web --session-signing-key session_signing_key \
-                            --tsa-host-key host_key \
-                            --tsa-authorized-keys authorized_worker_keys \
+/opt/concourse/concourse_linux_amd64 web --session-signing-key /opt/concourse/session_signing_key \
+                            --tsa-host-key /opt/concourse/host_key \
+                            --tsa-authorized-keys /opt/concourse/authorized_worker_keys \
                             --postgres-data-source {{ concourse_postgres_data_source }} \
                             --external-url {{ concourse_external_url }} \
                             --github-auth-client-id {{ concourse_github_auth_client_id }} \
